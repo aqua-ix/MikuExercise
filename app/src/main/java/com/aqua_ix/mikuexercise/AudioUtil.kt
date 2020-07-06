a163744a6c62a7a345274c2a9a215c02dea8e11e
Aqua-ix <souichi1997@gmail.com>
2020-07-07 03:17:07 +0900

    Update for other exercise mode

--- app/src/main/java/com/aqua_ix/mikuexercise/AudioUtil.kt
+++ app/src/main/java/com/aqua_ix/mikuexercise/AudioUtil.kt
@@ -11,6 +11,7 @@ class AudioUtil(private val context: Context) {
 
     companion object {
         private var instance: AudioUtil? = null
+
         @JvmStatic
         fun getInstance(context: Context): AudioUtil =
             instance ?: synchronized(this) {
@@ -35,7 +36,7 @@ class AudioUtil(private val context: Context) {
             .build()
     }
 
-    fun createSoundMap(vararg resources: Int){
+    fun createSoundMap(vararg resources: Int) {
         soundPool = getSoundPool(resources.size)
         for (resource in resources) {
             soundPool?.let {
@@ -46,7 +47,7 @@ class AudioUtil(private val context: Context) {
 
     fun playAudio(resource: Int) {
         soundPool?.setOnLoadCompleteListener { soundPool, _, status ->
-            if(status == 0) {
+            if (status == 0) {
                 soundMap[resource]?.let {
                     soundPool?.play(it, 1f, 1f, 1, 0, 1f)
                 }
--- app/src/main/java/com/aqua_ix/mikuexercise/MainActivity.kt
+++ app/src/main/java/com/aqua_ix/mikuexercise/MainActivity.kt
@@ -1,15 +1,9 @@
 package com.aqua_ix.mikuexercise
 
-import android.app.AlertDialog
-import android.app.Dialog
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.widget.NumberPicker
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.DialogFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -21,48 +15,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        absButton.setOnClickListener {
-            showDialog(this)
+
+        val items = arrayOf(
+            getString(R.string.title_text_situp),
+            getString(R.string.title_text_pushup),
+            getString(R.string.title_text_squat)
+        )
+        val adapter: ArrayAdapter<String> = ArrayAdapter(
+            this,
+            R.layout.dropdown_menu_popup_item,
+            items
+        )
+
+        title_menu_dropdown.setAdapter(adapter)
+
+
+        startButton.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java).putExtra(
+                "TIMES",
+                Integer.parseInt(title_times_input.text.toString())
+            )
+            startActivity(intent)
         }
 
         audioUtil = AudioUtil.getInstance(this)
         audioUtil.createSoundMap(R.raw.voice_watasito)
-
     }
 
     override fun onStart() {
         super.onStart()
         audioUtil.playAudio(R.raw.voice_watasito)
-    }
-
-    private fun showDialog(context: Context) {
-        class MainFragmentDialog : DialogFragment() {
-            override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-                var builder: AlertDialog.Builder? = null
-                activity?.let {
-                    val inflater: LayoutInflater = it.layoutInflater
-                    val view: View = inflater.inflate(R.layout.dialog, null, false)
-                    val np = view.findViewById(R.id.numberPicker) as NumberPicker
-                    np.minValue = 1
-                    np.maxValue = 100
-                    builder = AlertDialog.Builder(it)
-                    builder?.setTitle(context.getString(R.string.title_text_pic_times))
-                    builder?.setPositiveButton(
-                        "OK"
-                    ) { dialog, which ->
-                        val intent = Intent(context, ExerciseActivity::class.java)
-                            .putExtra("TIMES", np.value)
-                        startActivity(intent)
-                    }
-                    builder?.setNegativeButton("Cancel", null)
-                    builder?.setView(view)
-                }
-                return builder!!.create()
-            }
-        }
-        // Dialogの表示
-        val dialog = MainFragmentDialog()
-        dialog.show(supportFragmentManager, "times_setting_dialog")
     }
 }
 
